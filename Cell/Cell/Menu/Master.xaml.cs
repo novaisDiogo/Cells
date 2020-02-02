f965057330024417c7bbf97bce20284f36c659eb
Diogo Freitas <diogo.freitas@hotmail.com>
2020-02-02 20:45:40 -0300

    Adicionar arquivos de projeto.

---
+++ Cell/Cell/Menu/Master.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Cell.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoPagina1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
